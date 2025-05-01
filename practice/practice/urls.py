04914db7fc7c7741df3cbce3c4c1af03c2bbbbe6
WasiFahim <wasi.fahim02@gmail.com>
2025-05-01 16:16:32 +0600

    Home page setup

--- practice/practice/urls.py
+++ practice/practice/urls.py
@@ -18,10 +18,19 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
+
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('alumni/', views.index, name='index'),
+    path('', views.index, name='Alumni Home'),
     path('stories/', views.stories, name='stories'),
     path('alumni/', include('alumni.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
